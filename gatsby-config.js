4fc4775e5e53b6e12e967ae8cfd5fcf7fb9513d9
LeatherAndSenses <leatherandsenses@outlook.com>
2023-08-29 19:57:04 +0200

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `tutorial-site`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
